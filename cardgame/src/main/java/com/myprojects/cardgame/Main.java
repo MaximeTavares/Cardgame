7c454ac0a1a8d0a8d88bcc9d3f654d388f262b3c
Maxime Tavares <t.maxime59@gmail.com>
2025-10-23 17:18:10 +0200

    View added

--- cardgame/src/main/java/com/myprojects/cardgame/Main.java
+++ cardgame/src/main/java/com/myprojects/cardgame/Main.java
@@ -1,7 +1,14 @@
 package com.myprojects.cardgame;
 
+import com.myprojects.cardgame.controller.GameController;
+import com.myprojects.cardgame.model.Deck;
+import com.myprojects.cardgame.view.View;
+
 public class Main {
     public static void main(String[] args) {
+
+        GameController controller = new GameController(new Deck(), new View());
+        controller.run();
         
         
     }
---
+++ cardgame/src/main/java/com/myprojects/cardgame/view/View.java
@@ -0,0 +1,60 @@
+package com.myprojects.cardgame.view;
+
+import java.util.Scanner;
+
+import com.myprojects.cardgame.controller.GameController;
+
+public class View {
+
+	GameController controller;
+	Scanner keyboard = new Scanner(System.in);
+
+	public void setController(GameController gc) {
+		this.controller = gc;
+	}
+
+	public void promptForPlayerName() {
+		System.out.print("Enter player name (or 'start' to begin): ");
+		String name = keyboard.nextLine();
+		if (name.equalsIgnoreCase("start")) {
+			controller.startGame();
+		} else {
+			controller.addPlayer(name);
+		}
+	}
+
+	public void promptForFlip() {
+		System.out.print("Press Enter to flip cards...");
+		keyboard.nextLine();
+		controller.flipCards();
+	}
+
+	public void promptForNewGame() {
+		System.out.print("Play again? (y/n): ");
+		String response = keyboard.nextLine();
+		if (response.equalsIgnoreCase("y")) {
+			controller.startGame();
+		} else {
+			System.out.println("Thanks for playing!");
+			System.exit(0);
+		}
+	}
+
+	public void showWinner(Object name) {
+		System.out.println("The winner is: " + name + "!");
+	}
+
+	public void showPlayerName(int playerIndex, String playerName) {
+		System.out.println("Player " + playerIndex + " added: " + playerName);
+
+	}
+
+	public void showFaceDownCardForPlayer(int playerIndex, String playerName) {
+		System.out.println("Player " + playerIndex + " (" + playerName + ") has a face-down card.");
+	}
+
+	public void showCardForPlayer(int i, String playerName, String rank, String suit) {
+		System.out.println("Player " + i + " (" + playerName + ") flips: [" + rank + "][" + suit + "]");
+	};
+
+}
